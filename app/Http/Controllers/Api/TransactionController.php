abd5ae84f6baed6e81df922a29b37afddd134318
Otkelbay <160107106@stu.sdu.edu.kz>
2021-01-15 00:50:39 +0600

    add attributes to video

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -15,7 +15,10 @@ class TransactionController extends BaseController
                 'payment_type' => 'Qiwi',
                 'finished_at' => 1620822432,
                 'day_left' => 7,
-                'processed_at' => 1610454432
+                'processed_at' => 1610454432,
+                'currency' => '₸',
+                'finished' => '2021-01-11 11:11:11',
+                'processed' => '2021-01-11 11:11:11'
             ]
         ]);
     }
