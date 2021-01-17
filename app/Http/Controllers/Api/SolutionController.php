961d2dc3744c067d08f4d2a4eacb7b00836591ff
Otkelbay <160107106@stu.sdu.edu.kz>
2021-01-17 23:32:51 +0600

    receive category_ids as string

--- app/Http/Controllers/Api/SolutionController.php
+++ app/Http/Controllers/Api/SolutionController.php
@@ -12,7 +12,8 @@ class SolutionController extends BaseController
     public function courseSolutions(Course $course, Request $request)
     {
         $solutionsQuery = $course->solutions()->with('categories');
-        if ($categoryIds = $request->get('category_ids', [])) {
+        if ($categoryIds = $request->get('category_ids', '')) {
+            $categoryIds = is_array($categoryIds) ? $categoryIds : explode(',', $categoryIds);
             $solutionsQuery = $solutionsQuery->whereHas('categories', function (Builder $query) use ($categoryIds) {
                 $query->whereIn('categories.id', $categoryIds);
             });
